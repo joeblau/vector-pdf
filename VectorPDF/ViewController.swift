dae2a92738e4578c8fe2bcc4665c0ce84c37ca5c
Joe Blau <josephblau@gmail.com>
2017-12-26 20:49:14 -0800

    Added Vector PDF sample

--- VectorPDF/ViewController.swift
+++ VectorPDF/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let sliderInset: CGFloat = 10
+    private let imageDimension: CGFloat = 100
+    
+    private var multiplier = 1.0
+    private var slider: UISlider = UISlider()
+    private var vectorPdf: UIImageView = UIImageView(image: UIImage(named: "gdax"))
+    private var widthConstraint: NSLayoutConstraint?
+    private var heightConstraint: NSLayoutConstraint?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        vectorPdf.translatesAutoresizingMaskIntoConstraints = false
+        vectorPdf.contentMode = .scaleAspectFit
+        
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.minimumValue = 0.1
+        slider.maximumValue = 50.0
+        slider.addTarget(self, action: #selector(updateSliderScale), for: UIControlEvents.valueChanged)
+        
+        view.addSubview(vectorPdf)
+        view.addSubview(slider)
+        
+        configureViews()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func configureViews() {
+        vectorPdf.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        vectorPdf.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        widthConstraint = vectorPdf.widthAnchor.constraint(equalToConstant: imageDimension)
+        widthConstraint?.isActive = true
+        heightConstraint = vectorPdf.heightAnchor.constraint(equalToConstant: imageDimension)
+        heightConstraint?.isActive = true
+        
+        slider.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: sliderInset).isActive = true
+        slider.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -sliderInset).isActive = true
+        slider.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: sliderInset).isActive = true
     }
-
-
+    
+    // MARK: - Delegates
+    
+    @objc func updateSliderScale() {
+        widthConstraint?.constant = CGFloat(slider.value) * imageDimension
+        heightConstraint?.constant = CGFloat(slider.value) * imageDimension
+    }
 }
 
